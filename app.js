5127ed78fe0e3e75897c44f8439c5589e84aa751
pavankumarbvrm <pavankumarbvrm@gmail.com>
2024-01-25 13:02:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+/*
+
+GEREKLİ PAKETLER YÜKLENİYOR...
+
+*/
 var http = require('http');
 var express = require('express');
-var morgan = require('morgan');
-var fs = require('fs');  // Import the fs module
 
 var app = express();
-  
-app.set('port', process.env.PORT || 3005);
-app.set('views', __dirname + '/app/server/views');
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/app/public'));
-// Use morgan middleware to log requests to a file
-app.use(morgan('combined', { stream: fs.createWriteStream('/var/log/app.log', { flags: 'a' }) }));
 
-require('./app/routes')(app);
+app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
+app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
+app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
+app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
+
+require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
+
+/*
+
+HTTP SERVER OLUŞTURULDU
+
+*/
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
+});
